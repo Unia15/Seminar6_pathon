37f6fb1ae7a24448f266f7e1856c1cf2bc6c09cb
Julia.Ef <jullia.ef@yandex.ru>
2022-12-23 09:26:56 +0300

    Zadacha.py

---
+++ Zadacha2.py
@@ -0,0 +1,6 @@
+#Пример:
+#- пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+from math import factorial
+n = int(input('Введите число: '))
+print(list(map(lambda x: ((x == 1) and 1) or x * factorial(x -1),list(range(1,n+1)))))
